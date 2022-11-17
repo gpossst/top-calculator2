29b910c0bab0763cad68ea07939d69537489f2e5
Garrett Post <Gep100iosgames@gmail.com>
2022-11-17 16:56:51 -0500

    Rebuild entire calc

--- script.js
+++ script.js
@@ -1,92 +1,52 @@
-const add = document.querySelector('#add');
-const subtract = document.querySelector('#subtract');
-const multiply = document.querySelector('#multiply');
-const divide = document.querySelector('#divide');
-const outputContainer = document.querySelector('.outputs')
-let outputList = document.querySelector('#output-list')
-let operator = [];
-let outputs = [];
-let inputs =[]
+const screen = document.querySelector('.screen');
+let operator;
+let buttonInputs = [];
+let operands = [];
+
+
+
+
+
+
+function selectClear(){
+    operands = [];
+    buttonInputs =[];
+    operator = '';
+    populateScreen();
+}
+//clear all
+
+function inputString(buttonInputs) {
+    operands.unshift(buttonInputs.join(''));
+}
+//turns inputs array into a string to be shown on screen and used in operation
+
+function populateScreen() {
+    let text = buttonInputs.join('');
+    screen.innerText = text;
+}
+//puts selected number on screen
+
+function numberPress(num) {
+    buttonInputs.push(num);
+    populateScreen();
+}
+//press number buttons
 
 function selectAdd() {
-    if(inputs.length>0 && operator[0]=='add' && operator[0] != operator[1]){
-        selectReturn();
-    }
-    value();
-    operator.unshift('add');
-    console.log(inputs);
+    operator = 'add';
+    inputString(buttonInputs);
 }
-
 function selectSubtract() {
-    if(inputs.length>0 && operator[0]=='subtract' && operator[0] != operator[1]){
-        selectReturn();
-    }
-    value();
-    operator.unshift('subtract');
-    console.log(inputs);
+    operator = 'subtract';
+    inputString(buttonInputs);
 }
-
 function selectMultiply() {
-    if(inputs.length>0 && operator[0]=='multiply' && operator[0] != operator[1]){
-        selectReturn();
-    }
-    value();
-    operator.unshift('multiply');
-    console.log(inputs);
+    operator = 'multiply';
+    inputString(buttonInputs);
 }
-
 function selectDivide() {
-    if(inputs.length>0 && operator[0]=='divide' && operator[0] != operator[1]){
-        selectReturn();
-    }
-    value();
-    operator.unshift('divide');
-    console.log(inputs);
+    operator = 'divide';
+    inputString(buttonInputs);
 }
-
-function selectReturn() {
-    value();
-    valueOne = inputs[1];
-    valueTwo = inputs[0];
-    console.log(operator)
-    operate();
-    popAnswer();
-    inputs = [];
-}
-
-window.addEventListener('keypress', function(e) {
-    if(e.key === 'Enter') {
-        selectReturn();
-    }
-})
-
-function value(){
-    let value = document.getElementById('box-one').value;
-    inputs.unshift(value);
-}
-
-function popAnswer() {
-    document.getElementById('box-one').value = outputs[0];
-}
-
-function operate() {
-    if(operator[0] === 'add') {
-        outputs.unshift(+valueOne + +valueTwo);
-        console.log(outputs[0]);
-    } else if (operator[0] === 'subtract') {
-        outputs.unshift(+valueOne - +valueTwo);
-        console.log(outputs[0]);
-    } else if (operator[0] === 'multiply') {
-        outputs.unshift(+valueOne * +valueTwo);
-        console.log(outputs[0]);
-    } else if (operator[0] === 'divide') {
-        outputs.unshift(+valueOne / +valueTwo);
-        console.log(outputs[0]);
-    }
-}
-
-function clear() {
-    outputs.splice(0,outputs.length);
-    inputs.splice(0,inputs.length);
-    operator.splice(0,operator.length);
-}
+//switch to operators
