5d781814c106b1dd871358a7ead57a7313907c01
Garrett Post <Gep100iosgames@gmail.com>
2022-11-17 21:00:42 -0500

    Calculator correctly allows chaining

--- script.js
+++ script.js
@@ -3,6 +3,9 @@ let operator = [];
 let buttonInputs = [];
 let operands = [];
 let output;
+let pressed = 0;
+
+
 
 function operate(operands, operator) {
 
@@ -53,51 +56,65 @@ function populateScreen() {
 function numberPress(num) {
     buttonInputs.push(num);
     populateScreen();
+    pressed++;
 }
 //press number buttons
 
 function selectAdd() {
-    if (buttonInputs != '' || operator.length > 0) {
+    if (buttonInputs != ''|| operator.length > 0 && pressed > 0) {
     operator.unshift('add');
     }
     inputString(buttonInputs);
     buttonInputs = [];
-    if (operator[0] == operator[1]) {
+    if (operator.length > 1 && operator[0] == operator[1]) {
         inputString(buttonInputs);
         operate(operands, operator);
+    } else if (operator.length > 1 && operator[0] != operator[1]) {
+        inputString(buttonInputs);
+        operateBefore(operands, operator);
     }
 }
 function selectSubtract() {
-    if (buttonInputs != '' || operator.length > 0) {
+    if (buttonInputs != '' || operator.length > 0 && pressed > 0) {
         operator.unshift('subtract');
         }
     inputString(buttonInputs);
     buttonInputs = [];
-    if (operator[0] == operator[1]) {
+    if (operator.length > 1 && operator[0] == operator[1]) {
         inputString(buttonInputs);
         operate(operands, operator);
+    } else if (operator.length > 1 && operator[0] != operator[1]) {
+        inputString(buttonInputs);
+        operateBefore(operands, operator);
     }
 }
 function selectMultiply() {
-    if (buttonInputs != '' || operator.length > 0) {
+    if (buttonInputs != '' || operator.length > 0 && pressed > 0) {
         operator.unshift('multiply');
         }
     inputString(buttonInputs);
     buttonInputs = [];
-    if (operator[0] == operator[1]) {
+    if (operator.length > 1 && operator[0] == operator[1]) {
         inputString(buttonInputs);
         operate(operands, operator);
+    } else if (operator.length > 1 && operator[0] != operator[1]) {
+        inputString(buttonInputs);
+        operateBefore(operands, operator);
     }
 }
 function selectDivide() {
-    if (buttonInputs != '' || operator.length > 0) {
+    if (buttonInputs != '' || operator.length > 0 && pressed > 0) {
         operator.unshift('divide');
         }
     inputString(buttonInputs);
     buttonInputs = [];
-    if (operator[0] == operator[1]) {
+
+    if (operator.length > 1 && operator[0] == operator[1]) {
         inputString(buttonInputs);
         operate(operands, operator);
+    } else if (operator.length > 1 && operator[0] != operator[1]) {
+        inputString(buttonInputs);
+        operateBefore(operands, operator);
     }
 }
 //switch to operators
@@ -106,4 +123,28 @@ function selectDivide() {
 function selectReturn() {
     inputString(buttonInputs);
     operate(operands, operator);
+}
+
+function operateBefore(operands, operator) {
+    if(operator[1] == 'add'){
+        output = +operands[1] + +operands[0];
+        console.log(output);
+        operands.unshift(output);
+    } else if (operator[1] == 'subtract'){
+        output = +operands[1] - +operands[0];
+        console.log(output);
+        operands.unshift(output);
+    } else if (operator[1] == 'multiply'){
+        output = +operands[1] * +operands[0];
+        console.log(output);
+        operands.unshift(output);
+    } else if (operator[1] == 'divide'){
+        output = +operands[1] / +operands[0];
+        console.log(output);
+        operands.unshift(output);
+    }
+
+    buttonInputs = [];
+    operator = [];
+    screen.innerText = output
 }
